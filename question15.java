c929356227e9e61d933aa16dd82d57bef3b3d6ef
Buildwithaayush <iaayush.dev@gmail.com>
2026-03-06 21:29:44 +0545

    Update question15.java

--- question15.java
+++ question15.java
@@ -1,5 +1,15 @@
 package Workshop;
 
 public class question15 {
-
+  public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the first numbet:");
+		float a = in.nextFloat();
+		System.out.println("Enter the second number:");
+		float b = in.nextFloat();
+		System.out.println("Sum: " + ( a + b));
+		System.out.println("Difference: " + ( a - b));
+		System.out.println("Product: " + ( a * b));
+		System.out.println("Divide: " + ( a / b));
+	}
 }
